ec5349e60cedc2d026f274eb70ebdc7b6c0649bb
Mahmood Subhi <eng740@yahoo.com>
2018-08-02 21:44:25 -0400

    Mahmood

--- VMS/VMS/Controllers/HomeController.cs
+++ VMS/VMS/Controllers/HomeController.cs
@@ -12,8 +12,51 @@ namespace VMS.Controllers
     {
         public IActionResult Index()
         {
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult UserLogin()
+        {
             return View();
         }
+
+        [HttpPost]
+        public ViewResult UserLogin(UserLoginInput userLoginInput)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(userLoginInput);
+                return View("WellcomeBack", userLoginInput);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult MyAdministratorPage()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+
+        public ViewResult AboutVMS()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
 
         public IActionResult About()
         {
---
+++ VMS/VMS/Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+namespace VMS.Models
+{
+    public static class Repository
+    {
+        private static List<UserLoginInput> responses2 = new List<UserLoginInput>();
+        public static IEnumerable<UserLoginInput> Responses2
+        {
+            get
+            {
+                return responses2;
+            }
+        }
+        public static void AddResponse(UserLoginInput response)
+        {
+            responses2.Add(response);
+
+        }
+    }
+}
