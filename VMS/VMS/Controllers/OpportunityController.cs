3bb3cdddb0c3a38f7bc58c21ed6bc7d5436fe106
Robert Bolden <robert.bolden@gmail.com>
2018-08-06 19:11:15 -0400

    Added Access database to project in the Database folder

--- VMS/VMS/Controllers/OpportunityController.cs
+++ VMS/VMS/Controllers/OpportunityController.cs
@@ -25,10 +25,35 @@ namespace VMS.Controllers
 
             return View();
         }
+        /*
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonalDetail personalDetail = db.PersonalDetails.Find(id);
+            if (personalDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personalDetail);
+        }
+     
+        // POST: PersonalDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PersonalDetail personalDetail = db.Database1.Find(id);
+            db.PersonalDetails.Remove(personalDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
         //private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: PersonalDetails
-        /*
+      
         public ActionResult Index()
         {
             return View(db.PersonalDetails.ToList());
